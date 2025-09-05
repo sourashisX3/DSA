bb44e1880bb1fb427fb26fb12ecf52a9377ef457
sourashisd@notebrains.com <sourashisdas35816@gmail.com>
2025-09-05 23:49:50 +0530

    selection sort

--- SortingSearching/SelectionSort.java
+++ SortingSearching/SelectionSort.java
@@ -1,7 +1,30 @@
 package SortingSearching;
 
 public class SelectionSort {
-    static void selectionSort() {
 
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx])
+                    minIdx = j;
+            }
+            swap(arr, i, minIdx);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 64, 25, 12, 22, 11 };
+        selectionSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
