65fa6bf404ffba1ca82de8a02bf07a0a4e1b380e
sourashisd@notebrains.com <sourashisdas35816@gmail.com>
2025-09-11 22:54:42 +0530

    insertion sort prac

--- SortingSearching/InsertionSort.java
+++ SortingSearching/InsertionSort.java
@@ -1,5 +1,26 @@
 package SortingSearching;
 
 public class InsertionSort {
-    
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+        int key, j;
+
+        for (int i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 25, 0, 7, -1 };
+        insertionSort(arr);
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
 }
