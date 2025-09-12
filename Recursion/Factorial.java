b3305860fb5a2980127ff33474aeeb25ffa21f21
sourashisd@notebrains.com <sourashisdas35816@gmail.com>
2025-09-12 10:53:52 +0530

    fact

---
+++ Recursion/Factorial.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Factorial {
+
+    static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int res = factorial(5);
+        System.out.println(res);
+    }
+}
